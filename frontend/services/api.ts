5492c4c84e6581baf9eacb480dbb6fc33f906832
Ayush Ojha <ayushojha@Ayushs-MacBook-Air.local>
2026-05-26 15:03:22 +0530

    Fixed analyze endpoints

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -4,7 +4,7 @@ export async function analyzeFrame(blob: Blob) {
     formData.append('file', blob, 'frame.jpg')
 
     const response = await fetch(
-        'https://unmanufacturable-jodie-uncarted.ngrok-free.dev',
+        'https://unmanufacturable-jodie-uncarted.ngrok-free.dev/analyze',
         {
             method: 'POST',
             body: formData,
